c700097297be7ec0cb9edd21901230fef3cf4c45
zhangzhen <69829292+zzxingqiba@users.noreply.github.com>
2022-08-22 08:20:37 +0800

    test: 测试提交

--- play/app.vue
+++ play/app.vue
@@ -1,6 +1,6 @@
 <template>
   <wd-button type="xx">
-    <template #default>测试test</template>
+    <template #default>测试</template>
     <template #right-icon>
       <i class="wind-icon-caomei" />
     </template>
