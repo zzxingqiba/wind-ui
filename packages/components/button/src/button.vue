306a0726ef69033ff97cff9136e439a2c8c2db25
zhangzhen <69829292+zzxingqiba@users.noreply.github.com>
2022-08-23 18:16:18 +0800

    feat: 初始化util

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,19 +1,21 @@
 <template>
-  <button :class="type">
-    <slot name="default"></slot>
-    <slot name="right-icon"></slot>
+  <button :class="[]">
+    <span v-if="$slots.default"><slot /></span>
   </button>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
 import { buttonProps } from './button'
+import { useNamespace } from '@wind/hooks'
 export default defineComponent({
   name: 'WdButton',
   props: buttonProps,
   setup(props) {
     const _type = computed(() => props.type ?? '')
+    const ns = useNamespace('button')
     return {
+      ns,
       type: _type,
     }
   },
---
+++ packages/hooks/use-namespace/index.ts
@@ -0,0 +1,64 @@
+import { computed, unref } from 'vue'
+
+export const defaultNamespace = 'w'
+
+// B__E--M
+const _bem = (
+  namespace: string,
+  block: string,
+  blockSuffix: string,
+  element: string,
+  modifier: string
+) => {
+  let cls = `${namespace}-${block}`
+  if (blockSuffix) {
+    cls += `-${blockSuffix}`
+  }
+  if (element) {
+    cls += `__${element}`
+  }
+  if (modifier) {
+    cls += `--${modifier}`
+  }
+  return cls
+}
+
+export const useNamespace = (block: string) => {
+  const namespace = computed(() => defaultNamespace)
+
+  const baseName = () => _bem(unref(namespace), block, '', '', '')
+
+  const b = (blockSuffix = '') =>
+    _bem(unref(namespace), block, blockSuffix, '', '')
+  const e = (element?: string) =>
+    element ? _bem(unref(namespace), block, '', element, '') : ''
+  const m = (modifier?: string) =>
+    modifier ? _bem(unref(namespace), block, '', '', modifier) : ''
+  const be = (blockSuffix?: string, element?: string) =>
+    blockSuffix && element
+      ? _bem(unref(namespace), block, blockSuffix, element, '')
+      : ''
+  const em = (element?: string, modifier?: string) =>
+    element && modifier
+      ? _bem(unref(namespace), block, '', element, modifier)
+      : ''
+  const bm = (blockSuffix?: string, modifier?: string) =>
+    blockSuffix && modifier
+      ? _bem(unref(namespace), block, blockSuffix, '', modifier)
+      : ''
+  const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
+    blockSuffix && element && modifier
+      ? _bem(unref(namespace), block, blockSuffix, element, modifier)
+      : ''
+  // for css var
+  // --w_xxx: value;
+  return {
+    b,
+    e,
+    m,
+    be,
+    em,
+    bm,
+    bem,
+  }
+}
